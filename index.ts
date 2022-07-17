8ba2aa2a3d6a0cd4074c69e1abe3a304fb0514c8
noisyass2 <bernard.rivera.m@gmail.com>
2022-07-17 19:38:29 +0800

    fix: index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,24 @@
 import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
-import { promises as fs } from 'fs';
+import { promises as fs, rmSync } from 'fs';
 import { init as SOInit,  handleMessage as handleSOMessage } from "./sohandler";
 import { init as CCInit, handleMessage as handleCustomMessage } from "./customCommandHandler";
 import * as dotenv from 'dotenv';
-
+import express, { Express, Request, Response } from 'express';
+const app:Express = express();
 
 async function main() {
     // let auth = JSON.parse(await fs.readFile('./auth.js', 'utf-8'));
 	dotenv.config();
+	setupSOClipper();
 
 	let auth: IAuth = {
 		clientID : process.env.CLIENT_ID ?? "",
 		clientSecret : process.env.CLIENT_SECRET ?? ""
 	}
 	
-	console.log(auth);
-	console.log(process.env)
+	// console.log(auth);
+	// console.log(process.env)
 
     let settings = JSON.parse(await fs.readFile('./settings.json', 'utf-8'));
 	const clientId = auth.clientID;
@@ -33,7 +35,11 @@ async function main() {
     
     SOInit();
     CCInit();
-	const chatClient = new ChatClient({ authProvider, channels: [settings.channel] });
+	// let channels = [settings.channel]
+	// let channels = ['itsgillibean','speeeedtv', 'itschachatv']
+	let channels = settings.map((p:any) => p.channel);
+	console.log(channels)
+	const chatClient = new ChatClient({ authProvider, channels: channels });
 	await chatClient.connect();
 
 	chatClient.onMessage((channel, user, message) => {
@@ -50,11 +56,32 @@ async function main() {
 	chatClient.onSubGift((channel, user, subInfo) => {
 		chatClient.say(channel, `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
 	});
+
 }
 
 interface IAuth{
 	clientID: string;
 	clientSecret: string;
+}
+
+async function setupSOClipper() {
+	let appjs = await fs.readFile('./viewer/app.js', 'utf-8');
+	appjs = appjs.replace('{CLIENT_ID}', process.env.CLIENT_ID ?? "")
+	appjs = appjs.replace('{CLIENT_SECRET}', process.env.CLIENT_SECRET ?? "")
+
+	app.get('/',(req: Request, res: Response) => {
+		res.send("HELLO FROM BOT NG BAYAN!");
+	})
+
+	app.use(express.static('viewer'));
+
+
+	app.listen(process.env.PORT, () => {
+		console.log(`⚡️[server]: Server is running at https://localhost:${process.env.PORT}`);
+		
+	});
+
+
 }
 
 main();
